f6722974c850d6f0eb86ca3284a8996b12e614b5
NextFire <git@yuru.moe>
2024-02-02 14:10:34 -0500

    bump.sh

---
+++ bump.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -xe
+if [ -z "$1" ]; then
+  echo "Usage: $0 <new_version>"
+  exit 1
+fi
+
+git switch --detach
+NEW_VERSION=$(poetry version $1 --short)
+git commit -am $NEW_VERSION
+git tag $NEW_VERSION
+
+DEV_VERSION=$(poetry version patch --short --dry-run)-dev
+git switch main
+poetry version $DEV_VERSION
+git commit -am $DEV_VERSION
+
+git push origin $NEW_VERSION main
